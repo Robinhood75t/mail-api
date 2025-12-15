caa6cffe00b6605d73db349fb6af03df907016cb
Robin Kumar <161246802+Robinhood75t@users.noreply.github.com>
2025-12-15 16:59:34 +0530

    Update mailer configuration to use environment variables

--- config/mailer.js
+++ config/mailer.js
@@ -2,12 +2,12 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 dotenv.config();
 const transport = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
-    secure: true,
+    host: process.env.BREVO_SMTP_HOST,
+    port: process.env.BREVO_SMTP_PORT,
+    secure: false,
     auth : {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
+        user: process.env.BREVO_SMTP_USER,
+        pass: process.env.BREVO_SMTP_PASS
     },
 });
 
